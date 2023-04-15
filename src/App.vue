e93a0b16c89a4bda60410646af152244bf9a4ebf
isahua16 <isaelhuard@gmail.com>
2023-04-15 10:15:18 -0600

    Created a vue component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,33 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <product-hero></product-hero>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ProductHero from '@/components/ProductHero.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ProductHero
   }
 }
 </script>
 
 <style>
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/ProductHero.vue
@@ -0,0 +1,20 @@
+<template>
+    <header>
+    </header>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+    header {
+        width: 100%;
+        min-height: 50vh;
+        background-image: url("~@/assets/product.jpeg");
+        background-size: cover;
+        background-position: 50% 40%;
+    }
+</style>
